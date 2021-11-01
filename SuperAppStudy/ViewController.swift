645a7f207c5dfa7430f489046beb9ff67b847da0
Bradley.yoon <bradley.yoon@kakaocorp.com>
2021-11-01 13:42:55 +0900

    First commit

---
+++ SuperAppStudy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SuperAppStudy
+//
+//  Created by Bradley.yoon on 2021/11/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
